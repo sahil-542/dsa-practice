a6998a58141631aa35c0251586b0790ef5d9ef01
sahil-542 <sahilranjan898@gmail.com>
2026-08-04 19:49:29 +0530

    Time: 0 ms (100.00%) | Memory: 32.5 MB (46.32%) - LeetSync

---
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> findMissingElements(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+        int mx = *max_element(nums.begin(),nums.end());
+        int mn = *min_element(nums.begin(),nums.end());
+        vector<int>hash(mx+1,0);
+        vector<int>ans;
+        for(int i=0;i<n;i++){
+            hash[nums[i]]++;
+        }
+        for(int i=mn;i<=mx;i++){
+            if(hash[i] == 0) ans.push_back(i);
+        }
+        return ans;
+   }
+};

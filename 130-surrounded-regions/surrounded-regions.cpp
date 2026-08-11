7b6d0a51f24c0e9898db242e269fa824d2ef42c9
sahil-542 <sahilranjan898@gmail.com>
2026-08-11 21:57:29 +0530

    Time: 3 ms (21.27%) | Memory: 15 MB (23.75%) - LeetSync

---
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    void solve(vector<vector<char>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        queue<pair<int, int>> q;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i == 0 || j == 0 || i == n - 1 || j == m - 1) {
+                    if (grid[i][j] == 'O') {
+                        q.push({i, j});
+                        vis[i][j] = 1;
+                    }
+                }
+            }
+        }
+
+        while (!q.empty()) {
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+
+            int delrow[] = {-1, 0, 1, 0};
+            int delcol[] = {0, 1, 0, -1};
+
+            for (int i = 0; i < 4; i++) {
+                 
+                    int nrow = row + delrow[i];
+                    int ncol = col + delcol[i];
+
+                    if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
+                        grid[nrow][ncol] == 'O' && !vis[nrow][ncol]) {
+                        q.push({nrow, ncol});
+                        vis[nrow][ncol] = 1;
+                    }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 'O' && !vis[i][j]) {
+                    grid[i][j] = 'X';
+                }
+            }
+        }
+    }
+};
